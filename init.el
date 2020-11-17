cab3ffb619d6fa6b48c1f844e3f5cc2dca469d9a
Lee Runyon <LeeRuns@users.noreply.github.com>
2020-11-17 08:39:06 -0700

    Update init.el to prevent repeats
    the first  21 lines of lisp were repeated at the end.  According to emacs package initialize should only be run once.

--- init.el
+++ init.el
@@ -39,18 +39,7 @@
  '(package-selected-packages
    '(flycheck-pos-tip flycheck-clojure ox-reveal posframe processing-snippets processing-mode rg deadgrep flycheck-rust cargo racer yasnippet-classic-snippets rustic lsp-rust lsp-java company-lsp lsp-ui lsp-mode lsp yasnippet-snippets counsel-spotify exec-path-from-shell easy-kill auto-yasnippet org-pdfview pdf-tools atomic-chrome mingus simple-mpc pcre2el ag wgrep-ag wgrep haskell-mode aggressive-indent treemacs-projectile treemacs prodigy origami dumb-jump cider ggtags circe-notifications circe org-gcal mu4e-alert git-timemachine git-gutter magit hydra default-text-scale smartparens projectile auctex tern-auto-complete tern js2-refactor ac-js2 js2-mode emmet-mode web-mode iedit expand-region multiple-cursors hungry-delete beacon undo-tree virtualenvwrapper elpy flycheck doom-modeline doom-themes tao-theme poet-theme faff-theme zerodark-theme alect-themes moe-theme base16-theme zenburn-theme color-theme-modern company-jedi irony-eldoc company-irony company counsel ace-window htmlize noflet org-bullets which-key try use-package))
  '(sp-escape-quotes-after-insert nil))
-(setq max-lisp-eval-depth 10000)
-(setq max-specpdl-size 10000)
-(require 'package)
 
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
- 	     '("gnu" . "https://elpa.gnu.org/packages/"))
-;; (add-to-list 'package-archives
-;;	     '("melpa3" . "http://www.mirrorservice.org/sites/stable.melpa.org/packages/"))
-(package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
